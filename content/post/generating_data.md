c03dfa386ba2782aa45d77e56ae158081df4c6d5
lbarasti <lbarasti@users.noreply.github.com>
2020-04-28 00:01:10 +0100

    Generating data: first draft

--- content/post/generating_data.md
+++ content/post/generating_data.md
@@ -7,17 +7,103 @@ summary = ""
 +++
 
 ## Generating a realistic dataset
-We'll model the time between two visits as a random variable with exponential distribution and rate parameter of 15, meaning we expect to see around 15 users per unit of time.
+First, let's build ourselves a realistic dataset to work on. The data will represent users' visits to our website. This will include
+* timestamp: the relative time at which the visit happened
+* session duration: the amount of time the user spent on our website
+* device: what kind of device they were using to browse the website
+* age: their estimated age - this could be based on previous user's traffic
+* latitude and longitude: their location - this could be inferred by their IP address
+
+We'll represent timestamps as floating point values greater than zero, where the unit represents the time of the day. To reflect the fact that there is more traffic during the day, we'll model the arrival rate of users as exponential distributions with different rate parameter - let's say we expect on average 150 users per hour during the day, and 30 users per hour over the night.
 
 ```crystal
-arrived_after = Exponential.new(15)
+size = 5000 # sample size
+exp_day = Exponential.new(150)
+exp_night = Exponential.new(30)
+daylight = (7..22)
+```
 
-inter_arrival_time = (0...size).map { |el| arrived_after.rand }
-
-arrival_time = inter_arrival_time.reduce([] of Float64) { |cumulative, v|
-  cumulative << v + (cumulative.last? || 0)
+```crystal
+t = 0
+timestamp = (0...size).map {
+  t += daylight.includes?(t.to_i % 24) ? exp_day.rand : exp_night.rand
 }
 ```
+
+{{< figure src="/src/generating_data/01_visit_histogram.png" alt="A chart showing the number of visits in 1-hour buckets" caption="A chart showing the number of users' visits in 1-hour buckets. Notice the periodic pattern matching day and night." >}}
+
+For the session duration we'll make the assumption the time a user spends on the page is recorded in multiples of 15 seconds - so if a user stays on the page for less than 15 seconds, we'll record that as a session of length zero. We'll use a Poisson distribution with rate 1, and scale it by 15.
+
+```crystal
+poi = Poisson.new(1)
+session_duration = (0...size).map { poi.rand }.map { |v| v * 15 }
+```
+
+{{< figure src="/src/generating_data/02_session_duration.png" alt="A histogram showing the frequency of session durations" caption="A histogram showing the frequency of session durations." >}}
+
+The internet is a challenging place, a lot of people do leave our website very quickly. Luckily, we seem to have an audience of aficionados spending more than 30 seconds on our content.
+
+We'll pretend our users use one of three devices: mobile, desktop or tablet. Based on some market research, we'll assume that the split is around 65% mobile, 30% desktop and 5% tablet - yes, I guess our tablets are not "cool" anymore.
+
+```crystal
+threshold = [0.65, 0.95]
+device_type = ["mobile", "desktop", "tablet"]
+device = (0...size).map {
+  r = rand
+  idx = threshold.index { |t| r < t } || (device_type.size - 1)
+  device_type[idx]
+}
+```
+
+{{< figure src="/src/generating_data/03_device_type.png" alt="A histogram showing device usage frequency" caption="A histogram showing device usage frequency. Mobile phones seem to be the future..." >}}
+
+We'll use a similar strategy to generate age data. We'll assume our users are over 16 and never older than 60 - might be a limitation of our data provider - but we'll make it so that it's more likely to see younger users on the website.
+
+```crystal
+age_threshold = [0.6, 0.9]
+age_ranges = [{21, 3}, {30, 4}, {45, 7}]
+age_dist = age_ranges.map {|i| Normal.new(i.first.to_f, i.last.to_f)}
+
+age = (0...size).map {
+  r = rand
+  idx = age_threshold.index { |t| r < t } || age_threshold.size
+  age_dist[idx].rand.floor
+}
+```
+
+{{< figure src="/src/generating_data/04_age.png" alt="A scatter plot showing the age distribution" caption="A scatter plot showing the age distribution. Most of our users are aged between 16 and 35." >}}
+
+Finally, we'll generate latitude and longitude based on a world cities datafile (source: https://simplemaps.com/data/world-cities). I'll load the csv file in memory and then sample a number of cities from it:
+
+```crystal
+require "csv"
+world_cities = CSV.parse(File.read(File.join(__DIR__, "./worldcities.csv")))[1..]
+coords = world_cities.map(&.[2..3].map(&.to_f)).sample(size)
+```
+
+{{< figure src="/src/generating_data/05_location.png" alt="The graph represents the source of each visit to our website" caption="The graph represents the source of each visit to our website. We seem to be quite popular in the US!" >}}
+
+Note how simple it is to sample a number of values from an array - see `Array#sample`.
+
+We now have all our data in columnar format. We can persist the dataset to a csv file, for convenience.
+
+```crystal
+fake_data = timestamp.zip(
+  session_duration, device, age, coords.map(&.first), coords.map(&.last))
+
+File.open(File.join(__DIR__, "./data.csv"), "w") { |io|
+  CSV.build(io) { |csv|
+    csv.row ["timestamp", "session_duration", "device", "age", "lat", "lng"]
+    fake_data.each { |r|
+      csv.row r
+    }
+  }
+}
+```
+
+From this point on we go into analysis mode.
+
+## Exploring our dataset
 
 ##
 
--- static/src/generating_data/generating_data.cr
+++ static/src/generating_data/generating_data.cr
@@ -3,6 +3,11 @@ require "tablo"
 require "ishi"
 include Statistics::Distributions
 
+# Make the results reproduceable by fixing the random seed.
+R = Random.new(64)
+
+def rand; R.rand end
+
 # Turns a named tuple into tabular representation
 def table(data : NamedTuple)
   Tablo::Table.new(data.map { |k, v| [k, v] }, header_frequency: nil) { |t|
@@ -11,17 +16,24 @@ def table(data : NamedTuple)
   }
 end
 
-size = 100
-exp = Exponential.new(15)
+size = 5000
+exp_day = Exponential.new(150)
+exp_night = Exponential.new(30)
 poi = Poisson.new(1)
 
-inter_arrival_time = (0...size).map { exp.rand }
+# inter_arrival_time = (0...size).map { exp.rand }
 
-timestamp = inter_arrival_time[1..].reduce([inter_arrival_time.first]) { |cumulative, v|
-  cumulative << v + cumulative.last
+# timestamp = inter_arrival_time[1..].reduce([inter_arrival_time.first]) { |cumulative, v|
+#   cumulative << v + cumulative.last
+# }
+
+t = 0
+daylight = 7..22
+timestamp = (0...size).map {
+  t += daylight.includes?(t.to_i % 24) ? exp_day.rand : exp_night.rand
 }
 
-session_duration = (0...size).map { poi.rand }.map { |v| v * 30 }
+session_duration = (0...size).map { poi.rand }.map { |v| v * 15 }
 
 threshold = [0.65, 0.95]
 device_type = ["mobile", "desktop", "tablet"]
@@ -34,17 +46,59 @@ device = (0...size).map {
 freq = Statistics.frequency(device)
 device_freq = device_type.map { |t| freq[t] }
 xlabels = device_type.map_with_index { |v, i| {i.to_f, v} }.to_h
+
+age_threshold = [0.6, 0.9]
+age_ranges = [{21, 3}, {30, 4}, {45, 7}]
+age_dist = age_ranges.map {|i| Normal.new(i.first.to_f, i.last.to_f)}
+
 age = (0...size).map {
-  16 + rand(79)
+  r = rand
+  idx = age_threshold.index { |t| r < t } || age_threshold.size
+  age_dist[idx].rand.to_i
 }
+
+# Ishi.new do
+#   # 4.2 age distribution
+#   x, y = age.tally.map {|k, v| [k, v.to_f]}.transpose
+#   plot x, y
+# end
+
+require "csv"
+world_cities = CSV.parse(File.read(File.join(__DIR__, "./worldcities.csv")))
+coords = world_cities[1..].map(&.[2..3].map(&.to_f)).sample(size)
+
+# fake_data = timestamp.zip(
+#   session_duration, device, age, coords.map(&.first), coords.map(&.last))
+
+# File.open(File.join(__DIR__, "./data.csv"), "w") { |io|
+#   CSV.build(io) { |csv|
+#     csv.row ["timestamp", "session_duration", "device", "age", "lat", "lng"]
+#     fake_data.each { |r|
+#       csv.row r
+#     }
+#   }
+# }
 
 # Ishi.new do
 #   # 1 title: "cumulative total views over time"
 #   plot timestamp, (1..size).to_a, style: :lines
 # end
 
-# upper_timestamp = timestamp.last.ceil
-# bins = Statistics.bin_count(timestamp, bins: upper_timestamp.to_i, min: 0.0, max: upper_timestamp)
+upper_timestamp = timestamp.last.ceil
+bins = Statistics.bin_count(timestamp, bins: upper_timestamp.to_i, min: 0.0, max: upper_timestamp)
+window_size = 7
+
+sliding_count = bins.edges[1..-1].map { |e|
+  timestamp.count { |v| v <= e && v > e - window_size }
+}
+
+# Ishi.new do
+#   # 1.1 title: "sliding window"
+#   plot(bins.edges[1..-1], sliding_count, style: :lines)
+#     .yrange(0.0..sliding_count.max*1.1)
+# end
+
+
 # Ishi.new do
 #   # 1.2 title: "views per hour"
 #   plot(bins.edges, bins.counts, style: :lines)
@@ -61,6 +115,7 @@ age = (0...size).map {
 #   bins = Statistics.bin_count(session_duration, bins: session_duration.max)
 #   plot(bins.edges, bins.counts, style: :boxes)
 #     .yrange(0.0..bins.counts.max*1.1)
+#     .xtics((0..10).map { |t| {15.0 * t, "#{15.0 * t}"} }.to_h)
 #     .boxwidth(10)
 # end
 
@@ -73,20 +128,47 @@ age = (0...size).map {
 
 # Ishi.new do
 #   # 4.1 device vs age - scatter
-#   scatter(device.map{|x| device_type.index(x).not_nil!}, age, lc: "#4b03a1", lw: 3)
+#   device_idx = device.map { |x| device_type.index(x).not_nil! }
+#   edges = Statistics.bin_count(age, bins: 10, min: 17, max: 100).edges
+#   age_bins = age.map { |a| edges.find{ |t| a < t }.not_nil! }
+#   x,y,size = device_idx.zip(age_bins).tally.map {|k,v| [k[0].to_f, k[1], v / 25]}.transpose
+#   # pp device_idx.zip(age_bins).group_by(&.first).map {|d_id, users| users.uniq}
+#   scatter(x, y, pointsize: size, pt: "o", style: :points)
 #     .xtics(xlabels)
-#     .xrange(-1..4)
+#     .xrange(-1..3)
+#     .yrange(0..y.max.to_i)
 # end
 
+world = File.read(File.join(__DIR__, "./world.dat"))
+
+world_components =  world.split("\n\n")
+.reject(&.empty?)
+.map(&.lines
+  .map(&.split(/\s/)
+    .reject(&.empty?)
+    .map(&.to_f)))
+
+Ishi.new {
+  world_components.each { |component|
+  
+  x, y = component.transpose
+    plot(x, y, style: :lines, lc: "blue").show_key(false)
+  }
+  
+  # 5. world map
+  lat, lng = coords.transpose
+  scatter(lng, lat, pointsize: 0.3, pt: "o", lc: "red", style: :points)
+}
+
 # Data
-## timestamp | session duration | device | age
+## timestamp | session duration | device | age | lat | long
 ## introduce day / night pattern and/or peak hour
 ## 65% mobile, 30% desktop, 5% tablet
 ## skew age towards 25-34 (<16, 16-24, 25-34, 35-49, 50-65, >65)
 
 # Charts
 ##x 1. cumulative total views - time-series, line
-## 1.1 cumulative total views on sliding window - time-series, line
+##x 1.1 cumulative total views on sliding window - time-series, line
 ##x 1.2 views per hour - histogram, line
 
 ##x 2. session duration over time - time-series, line or step
@@ -94,4 +176,7 @@ age = (0...size).map {
 ##x 3. distribution of session duration - frequency / views histogram, bar
 
 ##x 4. device usage - histogram fequency vs device
+##x 4.2 age distribution
 ##x 4.1 device vs age - scatter
+
+##x 5. world map
